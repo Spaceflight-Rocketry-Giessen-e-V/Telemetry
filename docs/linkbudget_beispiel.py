27f3e89d0146c864ff2526500bc18e3ec3e6f427
Felix Seene <felix.seene@physik.uni-giessen.de>
2025-07-09 15:13:35 +0200

    update linkbudget beispiel

--- docs/linkbudget_beispiel.py
+++ docs/linkbudget_beispiel.py
@@ -1,25 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = 869.5e+6
+# Verbesserungen an der Berechnung:
+# - Fundierte Abschätzung L_TX und L_RX
+# - Festlegung auf Gewinn der Empfängerantenne
+# - Einbeziehung des realen Gewinns der Senderantenne
+# - Realistisches Höhenprofil
 
-P_TX = 27
-G_TX = 0
-P_RX = -118
-G_RX = 10
+f = 869.5e+6                # 869.5 MHz
 
-L_TX = 3
-L_RX = 3
-L_M = 6
+P_TX = 27                   # Datenblatt RC1780HP-RC232
+G_TX = 0                    # QFH-Antenne (tatsächlicher Gewinn ungleich 0 und abhängig vom Winkel)
+L_TX = 3                    # Abschätzung
+
+P_RX = -114                 # Datenblatt RC1780HP-RC232, alternativ: P_RX = -118
+G_RX = 10                   # Helixantenne, erste Abschätzung
+L_RX = 3                    # Abschätzung
 
 displacement = np.append(np.linspace(1000, 1000, 100), np.linspace(1000, 19000, 100))       # Horizontaler Abstand
 height = np.append(np.linspace(0, 9000, 100), np.linspace(9000, 0, 100))                    # Vertikaler Abstand
 distance = np.sqrt(height**2 + displacement**2)                                             # Abstand
 
-L_FS = 20 * np.log10((4 * np.pi * distance * f)/(2.998e+8))
-fade_margin = P_TX - P_RX + G_TX + G_RX - L_FS - L_TX - L_RX - L_M
+L_FS = 20 * np.log10((4 * np.pi * distance * f)/(2.998e+8))                                 # Freiraumdämpfung
 
-print('\nMinimale Fade Margin während des Flugprofils: ' + str(np.min(fade_margin)) + ' dB')
+L_M = P_TX - P_RX + G_TX + G_RX - L_FS - L_TX - L_RX                                        # Link Margin
+
+print('\nMinimale Link Margin während des Flugprofils: ' + str(np.min(L_M)) + ' dB')
 
 # Graphische Darstellung
 
@@ -35,14 +41,14 @@ plt.ylabel('Höhe (m)')
 plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(height[:100], fade_margin[:100], c = 'tab:orange', label = 'Ascent')
-plt.scatter(height[100], fade_margin[100], c = 'tab:green', zorder = 3, label = 'Apogee')
-plt.plot(height[100:], fade_margin[100:], c = 'tab:blue', label = 'Descent')
-plt.title('Fade Margin')
+plt.plot(height[:100], L_M[:100], c = 'tab:orange', label = 'Ascent')
+plt.scatter(height[100], L_M[100], c = 'tab:green', zorder = 3, label = 'Apogee')
+plt.plot(height[100:], L_M[100:], c = 'tab:blue', label = 'Descent')
+plt.title('Berechnung $L_\mathrm{M}$ (Link Margin)')
 plt.xlabel('Höhe (m)')
-plt.ylabel('Fade Margin (dB)')
+plt.ylabel('$L_\mathrm{M}$ (Link Margin) (dB)')
 plt.legend()
 
 plt.tight_layout()
 
-plt.savefig('linkbudget_beispiel.png', bbox_inches = 'tight')
+# plt.savefig('linkbudget_beispiel.png', bbox_inches = 'tight')
