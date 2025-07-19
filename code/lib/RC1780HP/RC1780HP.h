9b499de4136edfdf2be7df035d5a1bf45d69db90
Felix Seene <felix.seene@outlook.de>
2025-07-19 17:46:34 +0200

    kleines update

--- code/lib/RC1780HP/RC1780HP.h
+++ code/lib/RC1780HP/RC1780HP.h
@@ -14,8 +14,11 @@ class RC1780HP
 {
     public:
         RC1780HP(HardwareSerial* serialModule, uint8_t cfgpin, uint8_t rstpin, uint8_t ctspin, uint8_t rtspin);
+
         void begin(uint32_t baud_module);
+        
         void reset();
+
         uint8_t setChannel(uint8_t channel);
 
     private:
@@ -28,11 +31,14 @@ class RC1780HP
         uint8_t _channel;
         uint8_t _output_power;
         uint8_t _destination_address;
+
         uint16_t serialWait(uint32_t delay_microsecond);
         void serialFlush();
+
         uint8_t enterConfig();
         uint8_t exitConfig();
         uint8_t sendConfigCommand(uint8_t command);
+
         uint8_t readMemoryByte(uint8_t address, uint8_t* result);       // Private oder Public?
 };
 
