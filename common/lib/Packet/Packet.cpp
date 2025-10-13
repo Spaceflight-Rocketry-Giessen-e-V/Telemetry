7642e9e2bec1b4dab0a709dd765559f3b84a2771
Felix Seene <felix.seene@outlook.de>
2025-10-13 12:22:56 +0200

    update to package encoding/decoding fixes #7

--- common/lib/Packet/Packet.cpp
+++ common/lib/Packet/Packet.cpp
@@ -10,64 +10,50 @@
 void Packet::encode(uint8_t* packet, uint8_t address, uint8_t flight_mode, uint8_t low_power_mode, uint8_t subsystem_status, uint8_t status_events, float acceleration, float height_pressure, float height_gnss, float lat_gnss, float lon_gnss, float battery_voltage)
 {
     // Address
-    packet[0] |= address << 4;
-    
+    packet[0] |= (0x0F & address) << 4;
+
     // Status
-    packet[1] |= flight_mode << 7;
-    packet[1] |= low_power_mode << 6;
-    packet[1] |= subsystem_status << 5;
-    packet[1] |= (0b111 & status_events) << 2;
-    
+    packet[1] |= (0x01 & flight_mode) << 7;
+    packet[1] |= (0x01 & low_power_mode) << 6;
+    packet[1] |= (0x01 & subsystem_status) << 5;
+    packet[1] |= (0x07 & status_events) << 2;
+
     // Acceleration
-    if(acceleration < 0) 
+    if (acceleration < 0)
     {
-       packet[1] |= 1 << 1;
-       acceleration = -acceleration;
+        packet[1] |= 1 << 1;
+        acceleration = -acceleration;
     }
-    if(acceleration >= 31.9375)
+    if (acceleration >= 31.9375)
     {
-       packet[1] |= 1 << 0;
-       packet[2] = 0xFF;
+        acceleration = 31.9375;
     }
-    else
-    {
-        packet[2] = 0xFF & (uint16_t)(acceleration / 0.0625);
-        packet[1] |=  (0x0100 & (uint16_t)(acceleration / 0.0625)) >> 8;
-    }
-    
+    packet[1] |= (0x0100 & (uint16_t)(acceleration / 0.0625 + 0.5)) >> 8;
+    packet[2] |= 0x00FF & (uint16_t)(acceleration / 0.0625 + 0.5);
+
     // Height (Pressure)
-    if(0 < height_pressure && height_pressure < 65535 / 4 )
+    if (height_pressure < 0)
     {
-        packet[4] = 0xFF & (uint16_t)(height_pressure / 0.25);
-        packet[3] = (0xFF00 & (uint16_t)(height_pressure / 0.25)) >> 8;
-    } 
-    else if (height_pressure <= 0)
-    {
-        packet[4] = 0x00;
-        packet[3] = 0x00;
+        height_pressure = 0;
     }
-    else
+    else if (height_pressure > 16383.75)
     {
-        packet[4] = 0xFF;
-        packet[3] = 0xFF;
+        height_pressure = 16383.75;
     }
+    packet[3] |= (0xFF00 & (uint16_t)(height_pressure / 0.25 + 0.5)) >> 8;
+    packet[4] |= 0x00FF & (uint16_t)(height_pressure / 0.25 + 0.5);
 
     // Height (GNSS)
-    if(0 < height_gnss && height_gnss < 65535 / 4 )
+    if (height_gnss < 0)
     {
-        packet[6] = 0xFF & (uint16_t)(height_gnss / 0.25);
-        packet[5] = (0xFF00 & (uint16_t)(height_gnss / 0.25)) >> 8;
-    } 
-    else if (height_gnss <= 0)
-    {
-        packet[6] = 0x00;
-        packet[5] = 0x00;
+        height_gnss = 0;
     }
-    else
+    else if (height_gnss > 16383.75)
     {
-        packet[6] = 0xFF;
-        packet[5] = 0xFF;
+        height_gnss = 16383.75;
     }
+    packet[5] |= (0xFF00 & (uint16_t)(height_gnss / 0.25 + 0.5)) >> 8;
+    packet[6] |= 0x00FF & (uint16_t)(height_gnss / 0.25 + 0.5);
 
     // Lat (GNSS)
     if (lat_gnss < 0)
@@ -75,10 +61,10 @@ void Packet::encode(uint8_t* packet, uint8_t address, uint8_t flight_mode, uint8
         packet[7] |= 1 << 7;
         lat_gnss = -lat_gnss;
     }
-    packet[7] |= ((0xFF << 18) & (uint32_t)(lat_gnss / 0.0000026823)) >> 18;
-    packet[8] = ((0xFF << 10) & (uint32_t)(lat_gnss / 0.0000026823)) >> 10;
-    packet[9] = ((0xFF << 2) & (uint32_t)(lat_gnss / 0.0000026823)) >> 2;
-    packet[10] |= ((0xFF >> 6) & (uint32_t)(lat_gnss / 0.0000026823)) << 6;
+    packet[7] |= (0x1FC0000 & (uint32_t)(lat_gnss / 0.0000026823 + 0.5)) >> 18;
+    packet[8] |= (0x3FC00 & (uint32_t)(lat_gnss / 0.0000026823 + 0.5)) >> 10;
+    packet[9] |= (0x3FC & (uint32_t)(lat_gnss / 0.0000026823 + 0.5)) >> 2;
+    packet[10] |= (0x3 & (uint32_t)(lat_gnss / 0.0000026823 + 0.5)) << 6;
 
     // Lon (GNSS)
     if (lon_gnss < 0)
@@ -86,27 +72,24 @@ void Packet::encode(uint8_t* packet, uint8_t address, uint8_t flight_mode, uint8
         packet[10] |= 1 << 5;
         lon_gnss = -lon_gnss;
     }
-    packet[10] |= ((0xFF << 20) & (uint32_t)(lon_gnss / 0.00000134115)) >> 20;
-    packet[11] = ((0xFF << 12) & (uint32_t)(lon_gnss / 0.00000134115)) >> 12;
-    packet[12] = ((0xFF << 4) & (uint32_t)(lon_gnss / 0.00000134115)) >> 4;
-    packet[13] |= ((0xFF >> 4) & (uint32_t)(lon_gnss / 0.00000134115)) << 4;
-    
+    packet[10] |= (0x1F00000 & (uint32_t)(lon_gnss / 0.0000053645 + 0.5)) >> 20;
+    packet[11] |= (0xFF000 & (uint32_t)(lon_gnss / 0.0000053645 + 0.5)) >> 12;
+    packet[12] |= (0xFF0 & (uint32_t)(lon_gnss / 0.0000053645 + 0.5)) >> 4;
+    packet[13] |= (0x0F & (uint32_t)(lon_gnss / 0.0000053645 + 0.5)) << 4;
+
     // Battery Voltage
-    if( 5.4 < battery_voltage && battery_voltage < 8.4)
+    if (battery_voltage < 5.4)
     {
-        packet[13] |= 0x0F & (uint8_t)((battery_voltage - 5.4) / 0.2);
+        battery_voltage = 5.4;
     }
-    else if(battery_voltage <= 5.4)
+    else if (battery_voltage > 8.4)
     {
-        packet[13] |= 0x00;
+        battery_voltage = 8.4;
     }
-    else
-    {
-        packet[13] |= 0x0F;
-    }
+    packet[13] |= 0x0F & (uint8_t)((battery_voltage - 5.4) / 0.2 + 0.5);
 
     // End Byte
-    packet[14] = 0xEE;
+    packet[14] |= 0xEE;
 
     // COBS
     uint8_t cobs_byte = 0;
@@ -124,7 +107,7 @@ void Packet::encode(uint8_t* packet, uint8_t address, uint8_t flight_mode, uint8
 void Packet::decode(uint8_t* packet, uint8_t* address, uint8_t* flight_mode, uint8_t* low_power_mode, uint8_t* subsystem_status, uint8_t* status_events, float* acceleration, float* height_pressure, float* height_gnss, float* lat_gnss, float* lon_gnss, float* battery_voltage, float* rssi)
 {
     // End Byte
-    if(packet[14] != 0xEE)
+    if (packet[14] != 0xEE)
         return;
 
     // COBS
@@ -148,7 +131,7 @@ void Packet::decode(uint8_t* packet, uint8_t* address, uint8_t* flight_mode, uin
     *status_events = (packet[1] & 0x1C) >> 2;
 
     *acceleration = (float)((uint16_t)(packet[1] & 0x01) << 8 | (uint16_t)(packet[2])) * 0.0625;
-    if((packet[1] & 0x02) != 0)
+    if ((packet[1] & (0x01 << 1)) != 0)
         *acceleration = -*acceleration;
 
     *height_pressure = (float)((uint16_t)(packet[3]) << 8 | (uint16_t)(packet[4])) * 0.25;
@@ -156,11 +139,11 @@ void Packet::decode(uint8_t* packet, uint8_t* address, uint8_t* flight_mode, uin
     *height_gnss = (float)((uint16_t)(packet[5]) << 8 | (uint16_t)(packet[6])) * 0.25;
 
     *lat_gnss = (float)((uint32_t)(packet[7] & 0x7F) << 18 | (uint32_t)(packet[8]) << 10 | (uint32_t)(packet[9]) << 2 | (uint32_t)(packet[10] & 0xC0) >> 6) * 0.0000026823;
-    if((packet[7] & 0x80) != 0)
+    if ((packet[7] & (0x01 << 7)) != 0)
         *lat_gnss = -*lat_gnss;
 
-    *lon_gnss = (float)((uint32_t)(packet[10] & 0x1F) << 20 | (uint32_t)(packet[11]) << 12 | (uint32_t)(packet[12]) << 4 | (uint32_t)(packet[13] & 0xF0) >> 4) * 0.00000134115;
-    if((packet[10] & 0x20) != 0)
+    *lon_gnss = (float)((uint32_t)(packet[10] & 0x1F) << 20 | (uint32_t)(packet[11]) << 12 | (uint32_t)(packet[12]) << 4 | (uint32_t)(packet[13] & 0xF0) >> 4) * 0.0000053645;
+    if ((packet[10] & (0x01 << 5)) != 0)
         *lon_gnss = -*lon_gnss;
 
     *battery_voltage = (float)((uint8_t)(packet[13]) & 0x0F) * 0.2 + 5.4;
--- common/lib/Packet/examples/example.cpp
+++ common/lib/Packet/examples/example.cpp
@@ -16,29 +16,30 @@ void binary_print(uint8_t var)
 int main()
 {
     uint8_t packet[16] = { 0 };
-    uint8_t address = 0x04;
-    uint8_t flight_mode = 0b0;
-    uint8_t low_power_mode = 0b0;
+    uint8_t address = 0x01;
+    uint8_t flight_mode = 0b1;
+    uint8_t low_power_mode = 0b1;
     uint8_t subsystem_status = 0b111 == 0b111;
-    uint8_t status_events = 0b101;
-    float acceleration = -13.123;
-    float height_pressure = 9454.5 / 4;
-    float height_gnss = 531.213;
-    float lat_gnss = -50.587128;
+    uint8_t status_events = 0b011;
+    float acceleration = 21.32;
+    float height_pressure = 2000.2;
+    float height_gnss = 2363.5;
+    float lat_gnss = 50.587128;
     float lon_gnss = 8.683172;
-    float battery_voltage = 7.123;
+    float battery_voltage = 7.6;
     float rssi = -105.123;
 
-    std::cout << "Before transmitting:\n" << (uint16_t)address << " " << (uint16_t)flight_mode << " " << (uint16_t)low_power_mode << " " << (uint16_t)subsystem_status << " " << (uint16_t)status_events << " " << acceleration << " " << height_pressure << " " << height_gnss << " " << std::fixed << std::setprecision(6) << lat_gnss << " " << lon_gnss << std::defaultfloat << " " << battery_voltage << " " << rssi << std::endl;
-    
+    std::cout << "Before transmitting:\n" << (uint16_t)address << " " << (uint16_t)flight_mode << " " << (uint16_t)low_power_mode << " " << (uint16_t)subsystem_status << " " << (uint16_t)status_events << " " << acceleration << " " << height_pressure << " " << height_gnss << " " << std::fixed << std::setprecision(6) << lat_gnss << " " << lon_gnss << std::defaultfloat << " " << battery_voltage << " " << rssi << "\n\n";
+
     Packet::encode(packet, address, flight_mode, low_power_mode, subsystem_status, status_events, acceleration, height_pressure, height_gnss, lat_gnss, lon_gnss, battery_voltage);
     packet[15] = (uint8_t)(-2 * rssi);
+
+    std::cout << "Transmitted Packet:\n";
     for (int i = 0; i < 16; i++)
     {
         binary_print(packet[i]);
         printf(" ");
     }
-    printf("\n");
 
     address = 0;
     flight_mode = 0;
@@ -54,9 +55,8 @@ int main()
     rssi = 0;
 
     Packet::decode(packet, &address, &flight_mode, &low_power_mode, &subsystem_status, &status_events, &acceleration, &height_pressure, &height_gnss, &lat_gnss, &lon_gnss, &battery_voltage, &rssi);
-    
-    std::cout << "After receiving:\n" << (uint16_t)address << " " << (uint16_t)flight_mode << " " << (uint16_t)low_power_mode << " " << (uint16_t)subsystem_status << " " << (uint16_t)status_events << " " << acceleration << " " << height_pressure << " " << height_gnss << " " << std::fixed << std::setprecision(6) << lat_gnss << " " << lon_gnss << std::defaultfloat << " " << battery_voltage << " " << rssi << std::endl;
-    
-    printf("\n");
+
+    std::cout << "\n\nAfter receiving:\n" << (uint16_t)address << " " << (uint16_t)flight_mode << " " << (uint16_t)low_power_mode << " " << (uint16_t)subsystem_status << " " << (uint16_t)status_events << " " << acceleration << " " << height_pressure << " " << height_gnss << " " << std::fixed << std::setprecision(6) << lat_gnss << " " << lon_gnss << std::defaultfloat << " " << battery_voltage << " " << rssi << "\n\n";
+
     system("Pause");
 }
