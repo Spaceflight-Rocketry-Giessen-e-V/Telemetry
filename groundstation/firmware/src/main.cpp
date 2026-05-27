b094feac405502bfb0941b76a5c259da9a1fc779
Felix Seene <felix.seene@outlook.de>
2026-05-27 23:36:06 +0200

    create rough firmware outline
    fixes #49 fixes #50 Co-Authored-By: BenjaminBBBBBB <benjamin.bauersfeld@t-online> Co-Authored-By: Abakus2k <mail@pascalschlag.de> Co-Authored-By: Niklas-2911 <niklas_ow@proton.me>

--- groundstation/firmware/src/main.cpp
+++ groundstation/firmware/src/main.cpp
@@ -1,6 +1,5 @@
 /*
-    groundstation - main.cpp of the groundstation for the ASCENT II telemetry system.
-    Created by Felix Seene and Benjamin Bauersfeld
+    groundstation - main.cpp of the groundstation for the ASCENT III telemetry system.
     Spaceflight Rocketry Giessen e.V.
     Published under the CERN OHL-S v2 license at https://github.com/Spaceflight-Rocketry-Giessen-e-V/Telemetry.
 */
@@ -8,334 +7,41 @@
 #include "Arduino.h"
 #include "Radiocrafts_RC17xxHP_RC232.h"
 #include "Packet.h"
+#include "utility.h"
 
-// Pin assignment
-uint8_t ledpinR = PIN_PG5;
-uint8_t ledpinG = PIN_PG4;
-uint8_t ledpinB = PIN_PG3;
-uint8_t ledpin1 = PIN_PA0;  
-uint8_t ledpin2 = PIN_PG7;  
-uint8_t ledpin3 = PIN_PG6;  
-uint8_t ledpinRSSI1 = PIN_PD0; //(D27)
-uint8_t ledpinRSSI2 = PIN_PD1; //(D28)
-uint8_t ledpinRSSI3 = PIN_PD2; //(D29)
-uint8_t ledpinRSSI4 = PIN_PD3; //(D30)
-uint8_t ledpinRSSI5 = PIN_PD4; //(D31)
-uint8_t ledpinRSSI6 = PIN_PD5; //(D32)
-uint8_t ledpinRSSI7 = PIN_PD6; //(D33)
-uint8_t ledpinRSSI8 = PIN_PD7; //(D34)
-
-
-
-uint8_t D01pin = PIN_PA3;
-uint8_t D02pin = PIN_PA2;
-uint8_t D03pin = PIN_PA1;
-uint8_t D04pin = PIN_PA6;
-
-HardwareSerial* SerialPC1 = &Serial5;
-HardwareSerial* SerialPC2 = &Serial2;
-HardwareSerial* SerialModule = &Serial0;
-
-RC17xxHP_RC232 rc1780hp(SerialModule, D01pin, D04pin, D03pin, D02pin);
-
-// Data components received from sensors
-uint8_t temperature = 0;
-uint8_t subsystem_status = 0;
-uint8_t flight_mode = 0;
-uint8_t low_power_mode = 0;
-uint8_t status_events = 0;
-uint8_t send;
-uint8_t count = 0;
-float height_pressure = 0;
-float height_gnss = 0;
-float lat_gnss = 0;
-float lon_gnss = 0;
-float acceleration = 0;
-float battery_voltage = 8.4;
-float rssi = 0;
-uint32_t packet_time = 0;
-uint32_t time_since_last_packet = 0;
-
-// Incoming Data
-uint8_t packet[32] = {0};
-uint8_t incoming_Byte = 0;
-uint8_t index = 0;
-
-void setup()
+int main(void)
 {
-  pinMode(ledpinR, OUTPUT); // Red
-  pinMode(ledpinG, OUTPUT); // Green
-  pinMode(ledpinB, OUTPUT); // Blue
-  pinMode(ledpin1, OUTPUT); // Flight mode
-  pinMode(ledpin2, OUTPUT); // Low-power-mode
-  pinMode(ledpin3, OUTPUT); // Subsystem status
-  pinMode(ledpinRSSI1, OUTPUT); 
-  pinMode(ledpinRSSI2, OUTPUT);
-  pinMode(ledpinRSSI3, OUTPUT);
-  pinMode(ledpinRSSI4, OUTPUT);
-  pinMode(ledpinRSSI5, OUTPUT);
-  pinMode(ledpinRSSI6, OUTPUT);
-  pinMode(ledpinRSSI7, OUTPUT);
-  pinMode(ledpinRSSI8, OUTPUT);
+  init();
 
-  // Only RGB LED is turned on
-  digitalWrite(ledpinR, HIGH);
-  digitalWrite(ledpinG, HIGH);
-  digitalWrite(ledpinB, HIGH);
-  digitalWrite(ledpin1, LOW);
-  digitalWrite(ledpin2, LOW);
-  digitalWrite(ledpin3, LOW);
-  digitalWrite(ledpinRSSI1, LOW);
-  digitalWrite(ledpinRSSI2, LOW);
-  digitalWrite(ledpinRSSI3, LOW);
-  digitalWrite(ledpinRSSI4, LOW);
-  digitalWrite(ledpinRSSI5, LOW);
-  digitalWrite(ledpinRSSI6, LOW);
-  digitalWrite(ledpinRSSI7, LOW);
-  digitalWrite(ledpinRSSI8, LOW);
+  // Use RSSI LEDs As Setup Progress Indication
 
+  // Configuration Variables Declarations + Initialisations
 
-  SerialPC1->begin(115200);// Connection to PC
-  // SerialPC2->begin(19200);
-  SerialModule->pins(PIN_PA4, PIN_PA5);// Swap RX/TX pins for module
-  delay(100);
-  rc1780hp.begin(19200);
-  delay(100);
-  rc1780hp.ping();
+  // Pin Declarations
+  // Pin Initialisations
+  // UART Declarations
+
+  // Radiomodules Initialisations
+  // Radiomodules Configurations
   
-  //rc1780hp.memory_Reset();
-  while(rc1780hp.set_RF_DATA_RATE(0x05) != 0);
-  while(rc1780hp.set_RSSI_MODE(0x01) != 0);
-  while(rc1780hp.set_PACKET_TIMEOUT(0x00) != 0);
-  while(rc1780hp.set_PACKET_LENGTH(0x01) != 0);
-  while(rc1780hp.set_ADDRESS_MODE(0x00) != 0);
-  while(rc1780hp.set_CRC_MODE(0x00) != 0);
-  while(rc1780hp.set_LED_CONTROL(0x01) != 0);
-
-  rc1780hp.hard_Reset();
-  rc1780hp.serial_Flush();
-
-  digitalWrite(ledpinR, LOW);
-  digitalWrite(ledpinG, LOW);
-  digitalWrite(ledpinB, LOW);
-}
-
-void loop()
-{
-  // Send data to rocket
-  if(SerialPC1->available() != 0)
-  { 
-    send = SerialPC1->read(); 
-
-    // Conversion of capital letters in lowercase letters
-    if(send >= 'A' && send <= 'Z')
-    {
-      send = send - 'A' + 'a';
-    }
-
-    // Spam some parameters (to guarantee exchange during flight mode)
-    if(send == 'p' || send == 'f' || send == 'a' || send == 'b' || send == 'c' || send == 'd' || send == 'l' || send == 'm')
-    {
-      for (int z = 0; z < 8; z++)
-      {
-          if (send & (1 << z))
-          {
-              count++;
-          }
-      }
-      if (count % 2 != 0)
-      {
-          send |= 0x80;
-      }
-      SerialModule->write(send); 
-    }
-    else
-    {
-      for (int z = 0; z < 8; z++)
-      {
-          if (send & (1 << z))
-          {
-              count++;
-          }
-      }
-      if (count % 2 != 0)
-      {
-          send |= 0x80;
-      }
-      for(int i = 0; i < 25; i++)
-      {
-          delay(5);      
-          SerialModule->write(send); 
-      }
-    }
-    count = 0;
-  }
-
-  while(SerialModule->available() != 0)
+  // Data Arrays Preparations
+  
+  while(true)
   {
-    incoming_Byte = SerialModule->read();
-    packet[index] = incoming_Byte;
-    index++;
-
-    if(packet[index - 2] == 0xEE)
-    {
-      if(index >= 16)
-      {
-        Packet::decode(&packet[index - 16], &temperature, &subsystem_status, &flight_mode, &low_power_mode, &status_events, &acceleration, &height_pressure, &height_gnss, &lat_gnss, &lon_gnss, &battery_voltage, &rssi);
-        
-        SerialPC1->print("\ntemperature > 80 C: ");     SerialPC1->println(temperature);
-        SerialPC1->print("subsystem_status: ");         SerialPC1->println(subsystem_status);
-        SerialPC1->print("flight_mode: ");              SerialPC1->println(flight_mode);
-        SerialPC1->print("low_power_mode: ");           SerialPC1->println(low_power_mode);
-        SerialPC1->print("status_events: ");            SerialPC1->println(status_events);
-        SerialPC1->print("acceleration: ");             SerialPC1->println(acceleration,4);
-        SerialPC1->print("height_pressure: ");          SerialPC1->println(height_pressure);
-        SerialPC1->print("height_gnss: ");              SerialPC1->println(height_gnss);
-        SerialPC1->print("lat_gnss: ");                 SerialPC1->println(lat_gnss,7);
-        SerialPC1->print("lon_gnss: ");                 SerialPC1->println(lon_gnss,7);
-        SerialPC1->print("battery_voltage: ");          SerialPC1->println(battery_voltage);
-        SerialPC1->print("rssi: ");                     SerialPC1->println(rssi);
-        SerialPC1->print("time_since_last_packet: ");   SerialPC1->println(millis() - time_since_last_packet);
-      }
-      index = 0;
-      //memset(packet, 0, sizeof(packet));
-      time_since_last_packet = millis();
-    }
+    // Receiving Commands
+    // Process Commands (Execute Or Encode With Packet Library)
+    // Send Commands
     
-    if(index == 32)
-    {
-      index = 0;
-    }
-  }
- 
-  // LED5 glows wenn flight-mode is active
-  if(flight_mode == 1)
-  {
-    digitalWrite(ledpin1, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledpin1, LOW);
+    // Check Buttons
+    // Check DSUB-9 Connector
+
+    // Receiving Data Packets
+    // Decode Data Packets (With Packet Library)
+    // Send Data To Computer
+
+    // Update LEDs
+    // Update Display
   }
 
-  // LED6 glows wenn low-power-mode is active
-  if(low_power_mode == 1)
-  {
-    digitalWrite(ledpin2, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledpin2, LOW);
-  }
-
-  // LED7 glows wenn subsystems are ready
-  if(subsystem_status == 0b11)
-  {
-    digitalWrite(ledpin3, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledpin3, LOW);
-  }
-  
-  // RGB LED (RSSI)
-  if(rssi > -40)// High signal strength
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, HIGH);
-    digitalWrite(ledpinRSSI4, HIGH);
-    digitalWrite(ledpinRSSI5, HIGH);
-    digitalWrite(ledpinRSSI6, HIGH);
-    digitalWrite(ledpinRSSI7, HIGH);
-    digitalWrite(ledpinRSSI8, HIGH);
-  }
-  else if(rssi > -50)
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, HIGH);
-    digitalWrite(ledpinRSSI4, HIGH);
-    digitalWrite(ledpinRSSI5, HIGH);
-    digitalWrite(ledpinRSSI6, HIGH);
-    digitalWrite(ledpinRSSI7, HIGH);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else if(rssi > -60)
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, HIGH);
-    digitalWrite(ledpinRSSI4, HIGH);
-    digitalWrite(ledpinRSSI5, HIGH);
-    digitalWrite(ledpinRSSI6, HIGH);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else if(rssi > -70)// Medium signal strength
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, HIGH);
-    digitalWrite(ledpinRSSI4, HIGH);
-    digitalWrite(ledpinRSSI5, HIGH);
-    digitalWrite(ledpinRSSI6, LOW);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else if(rssi > -80)
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, HIGH);
-    digitalWrite(ledpinRSSI4, HIGH);
-    digitalWrite(ledpinRSSI5, LOW);
-    digitalWrite(ledpinRSSI6, LOW);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else if(rssi > -90)
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, HIGH);
-    digitalWrite(ledpinRSSI4, LOW);
-    digitalWrite(ledpinRSSI5, LOW);
-    digitalWrite(ledpinRSSI6, LOW);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else if(rssi > -100) // Low signal strength
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, HIGH);
-    digitalWrite(ledpinRSSI3, LOW);
-    digitalWrite(ledpinRSSI4, LOW);
-    digitalWrite(ledpinRSSI5, LOW);
-    digitalWrite(ledpinRSSI6, LOW);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else if(rssi > -110)
-  {
-    digitalWrite(ledpinRSSI1, HIGH);
-    digitalWrite(ledpinRSSI2, LOW);
-    digitalWrite(ledpinRSSI3, LOW);
-    digitalWrite(ledpinRSSI4, LOW);
-    digitalWrite(ledpinRSSI5, LOW);
-    digitalWrite(ledpinRSSI6, LOW);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
-  else
-  {
-    digitalWrite(ledpinRSSI1, LOW);
-    digitalWrite(ledpinRSSI2, LOW);
-    digitalWrite(ledpinRSSI3, LOW);
-    digitalWrite(ledpinRSSI4, LOW);
-    digitalWrite(ledpinRSSI5, LOW);
-    digitalWrite(ledpinRSSI6, LOW);
-    digitalWrite(ledpinRSSI7, LOW);
-    digitalWrite(ledpinRSSI8, LOW);
-  }
+  return 0;
 }
--- onboard/firmware/src/main.cpp
+++ onboard/firmware/src/main.cpp
@@ -1,6 +1,5 @@
 /*
-    onboard - main.cpp of the board computer for the ASCENT II telemetry system.
-    Created by Felix Seene and Benjamin Bauersfeld
+    onboard - main.cpp of the board computer for the ASCENT III telemetry system.
     Spaceflight Rocketry Giessen e.V.
     Published under the CERN OHL-S v2 license at https://github.com/Spaceflight-Rocketry-Giessen-e-V/Telemetry.
 */
@@ -9,492 +8,51 @@
 #include "Wire.h"
 #include "Radiocrafts_RC17xxHP_RC232.h"
 #include "Packet.h"
+#include "i2c_connectivity.h"
+#include "utility.h"
 
-// Status events
-// 0: 50 m
-// 1: 100 m
-// 2: 150 m
-// 3: 200 m
-// 4: Armed
-// 5: Liftoff detected
-// 6: Booster burnout detected
-// 7: Drogue deployed (apogee)
-// 8: Drogue deployed (timer)
-// 9: Drogue deployed (command)
-// 10: Main deployed (altitude)
-// 11: Main deployed (timer)        // Not used
-// 12: Main deployed (command)
-// 13: Landing detected             // Not used
-
-// Parity check
-uint8_t incoming_data = 0;
-uint8_t count = 0;
-
-// Configuration
-const uint8_t time_between_packets_standby = 15;                                              // in seconds   In standby, data packets are sent every 30 s   
-const uint8_t hz = 8;                                                                         // in Hz        During flight, 8 Hz (125 ms interval)
-const uint32_t flight_mode_max_duration = 360 - 3600 / time_between_packets_standby / hz;     // in seconds   6 min (360s) is max sending time
-
-// Pin assignment
-uint8_t ledpinR = PIN_PF0; // Red
-uint8_t ledpinG = PIN_PE7; // Green
-uint8_t ledpinB = PIN_PE6; // Blue
-uint8_t ledpin1 = PIN_PF3; // Status
-uint8_t ledpin2 = PIN_PF2; // Flight mode
-uint8_t ledpin3 = PIN_PF1; // Not used so far
-uint8_t ledpinDEBUG1 = PIN_PB6; // Not used so far
-uint8_t ledpinDEBUG2 = PIN_PB7; // Not used so far
-uint8_t ledpinSENS = PIN_PD4; // Sensor circuit board 1
-uint8_t ledpinIGN = PIN_PD2; // Sensor circuit board 2
-uint8_t ledpinPWR = PIN_PD3; // Landing systems, not used so far
-uint8_t d4pin = PIN_PC6; // Not used so far
-uint8_t d5pin = PIN_PC7; // Not used so far
-uint8_t d26pin = PIN_PF4; // Not used so far
-uint8_t d27pin = PIN_PF5; // Not used so far
-uint8_t d28pin = PIN_PF6; // Not used so far
-uint8_t arm1pin = PIN_PG1;
-uint8_t slppin = PIN_PG0;
-
-uint8_t buzzerpin = PIN_PB0; // Not used so far
-
-uint8_t cfg1pin = PIN_PA5;
-uint8_t rst1pin = PIN_PG7;
-uint8_t cts1pin = PIN_PA3;
-uint8_t rts1pin = PIN_PA4;
-
-uint8_t cfg2pin = PIN_PB3; // Not used so far
-uint8_t rst2pin = PIN_PG6; // Not used so far
-uint8_t cts2pin = PIN_PB1; // Not used so far
-uint8_t rts2pin = PIN_PB2; // Not used so far
-
-HardwareSerial* SerialPC = &Serial4;
-HardwareSerial* SerialUmbilical = &Serial1; // Not used so far
-HardwareSerial* SerialModule1 = &Serial0;
-HardwareSerial* SerialModule2 = &Serial3; // Not used so far
-
-// Radio module initialisation
-RC17xxHP_RC232 rc1780hp(SerialModule1, cfg1pin, rst1pin, cts1pin, rts1pin);
-
-// Functions declaration
-void get_packet_data();
-void send_packet();
-
-// Data components
-int8_t temperature = 0;
-uint8_t subsystem_status = 0b000;
-uint8_t flight_mode = 0;
-uint8_t low_power_mode = 0;
-uint8_t i2c_connections = 0b000;
-uint8_t status_events = 0;
-float height_pressure = 0;
-float height_gnss = 0;
-float lat_gnss = 0;
-float lon_gnss = 0;
-float acceleration = 0;
-float battery_voltage = 0;
-
-void setup()
+int main(void)
 {
-  pinMode(ledpinR, OUTPUT);
-  pinMode(ledpinG, OUTPUT);
-  pinMode(ledpinB, OUTPUT);
-  pinMode(ledpin1, OUTPUT);
-  pinMode(ledpin2, OUTPUT);
-  pinMode(ledpin3, OUTPUT);
-  pinMode(ledpinDEBUG1, OUTPUT);
-  pinMode(ledpinDEBUG2, OUTPUT);
-  pinMode(ledpinSENS, OUTPUT);
-  pinMode(ledpinIGN, OUTPUT);
-  pinMode(ledpinPWR, OUTPUT);
-  pinMode(arm1pin, OUTPUT);
-  pinMode(slppin, OUTPUT);
+  init();
 
-  // Only LED1 (power indicator) and RGB LED are turned on
-  digitalWrite(ledpinR, HIGH);
-  digitalWrite(ledpinG, LOW);
-  digitalWrite(ledpinB, LOW);
-  digitalWrite(ledpin1, HIGH);
-  digitalWrite(ledpin2, LOW);
-  digitalWrite(ledpin3, LOW);
-  digitalWrite(ledpinDEBUG1, LOW);
-  digitalWrite(ledpinDEBUG2, LOW);
-  digitalWrite(ledpinSENS, LOW);
-  digitalWrite(ledpinIGN, LOW);
-  digitalWrite(ledpinPWR, HIGH);
+  // Buzzersound On Bootup #1
 
-  // Initialize UART
-  //SerialPC->begin(115200);
-  SerialPC->pins(PIN_PE4, PIN_PE5);// Swap RX/TX pins for radio module uart connection
-  SerialUmbilical->pins(PIN_PE4, PIN_PE5);// Swap RX/TX pins for radio module uart connection
-  SerialModule1->pins(PIN_PA0, PIN_PA1);// Swap RX/TX pins for radio module uart connection
-  SerialModule2->pins(PIN_PC0, PIN_PC1);// Swap RX/TX pins for radio module uart connection
- 
-  // Initialize I2C
-  Wire.pins(PIN_PC2, PIN_PC3);
-  Wire.begin();
+  // Configuration Variables Declarations + Initialisations (Standby Packet Frequency, Loop Packet Frequency etc.)
+
+  // Pin Declarations
+  // Pin Initialisations
+  // UART Declarations
+  // I2C Declarations
+
+  // Buzzersound On Bootup #2
+
+  // Radiomodules Initialisations
+  // Radiomodules Configurations
+
+  // Buzzersound On Bootup #3
   
-  // Initialize radio transceiver and wait until communication is established
-  delay(3.2 * 50); // Necessary delay: t_{OFF-IDLE} = 3.2, safety factor 10
-  rc1780hp.begin(19200);
-  delay(3.2 * 50); // Necessary delay: t_{OFF-IDLE} = 3.2, safety factor 10
-  rc1780hp.ping();
+  // Data Arrays Preparations
+  // Subsystems Initialisations
 
-  digitalWrite(ledpinG, HIGH);
+  // Buzzersound On Bootup #4
 
-  // Before each flight memory is reset and non-standard settings are reconfigured
-  //while(rc1780hp.memory_Reset() != 0);
-  while(rc1780hp.set_RF_DATA_RATE(0x05) != 0);
-  while(rc1780hp.set_PACKET_TIMEOUT(0x00) != 0);
-  while(rc1780hp.set_PACKET_END_CHARACTER(0xEE) != 0);
-  while(rc1780hp.set_ADDRESS_MODE(0x00) != 0);
-  while(rc1780hp.set_CRC_MODE(0x00) != 0);
-  while(rc1780hp.set_LED_CONTROL(0x01) != 0);
-  rc1780hp.hard_Reset();
-  rc1780hp.serial_Flush();
-}
-
-uint32_t loop_start_time;
-uint8_t loop_count = 0;
-uint8_t led_switch = 0;
-uint32_t flight_mode_start_time;
-
-void loop()
-{
-  loop_start_time = millis();
-
-  // Check for incoming data from groundstation
-  if(SerialModule1->available() != 0)
-  {
-    // Parity check
-    incoming_data = SerialModule1->read();
-    for (int z = 0; z < 8; z++) 
-    {
-        if (incoming_data & (1 << z))
-        {
-            count++;
-        }
-    }
-    if (count % 2 == 0)
-    {
-        incoming_data &= 0x7F;
-    }
-    count = 0;
-
-    // Process incoming serial commands from the groundstation
-    // The command reference can be found under docs/commands.md
-    switch (incoming_data)
-    {
-      // Flight computer ping
-
-      case 'p':
-        break;
-
-      // Main parachute height adjustment
-
-      case 'a': // 50 m
-        Wire.beginTransmission(0x40);
-        Wire.write('a');
-        Wire.endTransmission();
-        break;
-      
-      case 'b': // 100 m
-        Wire.beginTransmission(0x40);
-        Wire.write('b');
-        Wire.endTransmission();
-        break;
-
-      case 'c': // 150 m
-        Wire.beginTransmission(0x40);
-        Wire.write('c');
-        Wire.endTransmission();
-        break;
-
-      case 'd': // 200 m
-        Wire.beginTransmission(0x40);
-        Wire.write('d');
-        Wire.endTransmission();
-        break;
-      
-      // Low power mode 
-
-      case 'l': // activation
-        if(low_power_mode == 0)
-        {
-          digitalWrite(slppin, HIGH);
-          digitalWrite(ledpin1, LOW);
-          digitalWrite(ledpinR, LOW);
-          digitalWrite(ledpinG, LOW); 
-          digitalWrite(ledpinB, LOW);
-          digitalWrite(ledpin2, LOW); 
-          digitalWrite(ledpinSENS, LOW); 
-          digitalWrite(ledpinIGN, LOW); 
-          digitalWrite(ledpinPWR, LOW); 
-          low_power_mode = 1;
-        }
-        break;
-      
-      case 'm': // deactivation
-        if(low_power_mode == 1)
-        {
-          digitalWrite(slppin, LOW);
-          low_power_mode = 0;
-        }
-        break;
-      
-      // Flight mode 
-
-      case 'f': // activation
-        if(flight_mode == 0)
-        {
-          digitalWrite(arm1pin, HIGH);
-          // delay(1);
-          //if(digitalRead(d3pin) == HIGH)  This connection is not planned anymore??
-          // {
-            digitalWrite(ledpin2, HIGH);
-            flight_mode_start_time = millis();
-            flight_mode = 1;
-            Wire.beginTransmission(0x40);
-            Wire.write('f');
-            Wire.endTransmission();
-          // }
-          // else
-          // {
-          //   digitalWrite(arm1pin, LOW);
-          // }
-        }
-        break;
-      #if(0)
-      case 'g': // deactivation
-        if(flight_mode == 1)
-        {
-          digitalWrite(arm1pin, LOW);
-          delay(1);
-          if(digitalRead(d3pin) == LOW)
-          {
-            digitalWrite(ledpin2, LOW);
-            flight_mode = 0;
-            Wire.beginTransmission(0x40);
-            Wire.write('g');
-            Wire.endTransmission();
-          }
-          else
-          {
-            digitalWrite(arm1pin, HIGH);
-          }
-        }
-        break;
-#endif
-
-      // Drogue parachute ejection
-
-      case 'q':
-        Wire.beginTransmission(0x40);
-        Wire.write('q');
-        Wire.endTransmission();
-        break;
-
-      // Main parachute ejection
-
-      case 'r':
-        Wire.beginTransmission(0x40);
-        Wire.write('r');
-        Wire.endTransmission();
-        break;
-      
-      // Unknown command
-
-      default: 
-        break;
-    }
-    
-    send_packet();
-  }
-
-  // Update variables from i2c connected systems at the beginning of each cycle
-  get_packet_data();
-
-  // Check if the other boards are connected and ready (When a system is connected or ready, a 1 is written to the respective position. E.g. 0b111 then means that all are connected)
-  if(low_power_mode == 0)
-  {
-    if(flight_mode == 1)
-    {
-      digitalWrite(ledpin2, HIGH);
-    }
-    else
-    {
-      digitalWrite(ledpin2, LOW);
-    }
-
-    digitalWrite(ledpin1, 1 - led_switch);
-
-    // Sensor circuit board 1
-    if((i2c_connections & 0b001) != 0)
-    {
-      if((subsystem_status & 0b001) != 0)
-      {
-        digitalWrite(ledpinSENS, HIGH);
-      }
-      else
-      {
-        digitalWrite(ledpinSENS, led_switch);
-      }
-    }
-    else
-    {
-      digitalWrite(ledpinSENS, LOW);
-    }
-
-    // Sensor circuit board 2
-    if((i2c_connections & 0b010) != 0)
-    {
-      if((subsystem_status & 0b010) != 0)
-      {
-        digitalWrite(ledpinIGN, HIGH);
-      }
-      else
-      {
-        digitalWrite(ledpinIGN, led_switch);
-      }
-    }
-    else
-    {
-      digitalWrite(ledpinIGN, LOW);
-    }
-
-    // POWER UNIT (no microcontroller yet)
-    digitalWrite(ledpinPWR, HIGH);
-
-    // All boards connected but not ready (RGB_LED turns blue)
-    if(i2c_connections == 0b111 && subsystem_status != 0b111)
-    {
-      digitalWrite(ledpinR, LOW);
-      digitalWrite(ledpinG, LOW);
-      digitalWrite(ledpinB, HIGH);
-    }
-    // All connected and ready (RGB_LED turns green)
-    else if(i2c_connections == 0b111 && subsystem_status == 0b111)
-    {
-      digitalWrite(ledpinR, LOW);
-      digitalWrite(ledpinG, HIGH);
-      digitalWrite(ledpinB, LOW);
-    }
-    // Else
-    else
-    {
-      digitalWrite(ledpinR, HIGH);
-      digitalWrite(ledpinG, LOW);
-      digitalWrite(ledpinB, LOW);
-    }
-
-    led_switch = 1 - led_switch;
-  }
-
-  // Send data to groundstation
-  if(flight_mode == 1)
-  {
-    if(millis() - flight_mode_start_time > flight_mode_max_duration * 1000) // If the maximum legal transmission time is reached, the flight mode must be turned off
-    {
-      flight_mode = 0;
-    }
-    send_packet();
-  }
-  else if(loop_count % (time_between_packets_standby * hz) == 0) // Sending in non-flight mode
-  {
-    send_packet();
-    loop_count = 0;
-  }
+  // Loop Variables Declarations + Initialisations (Loop Count, Loop Start Time etc.)
   
-  delay(1000 / hz - (millis() - loop_start_time)); // Waits until the iteration has taken 125ms
-  loop_count++;
-}
+  while(true)
+  {
+    // Subsystem Connection Check
 
-void get_packet_data()
-{
-  // If sensor board not yet confirmed connected, test I²C response
-  if((i2c_connections & 0b001) == 0)
-  {
-      Wire.beginTransmission(0x20);                       // Sensor circuit board 1
-      i2c_connections |= ((Wire.endTransmission() == 0) << 0); 
-  }
-  if((i2c_connections & 0b010) == 0)
-  {
-      Wire.beginTransmission(0x30);                       // Sensor circuit board 2
-      i2c_connections |= ((Wire.endTransmission() == 0) << 1); 
-  }
-  if((i2c_connections & 0b100) == 0)
-  {
-      Wire.beginTransmission(0x40);                       // Landing systems
-      i2c_connections |= ((Wire.endTransmission() == 0) << 2); 
+    // Subsystem Data Request
+
+    // Receiving Commands (Packet Library Function)
+    // Execute Commands (Distribute Data To Subsystems etc.)
+
+    // Sending Data (Telemetry Packet Or Sensorics Packet)
+    // Save Data On Flash Chip
+
+    // Delay
+    // Update Loop Variables
   }
 
-  uint32_t tmp;
-
-  // Sensor circuit board 1: status (1 Byte), height pressure (4 Bytes), GNSS height (4 Bytes), GNSS Lat (4 Bytes) + Lon (4 Bytes)
-  if((i2c_connections & 0b001) != 0)
-  {
-    Wire.requestFrom(0x20, 17); 
-    uint8_t result[17];
-    for(int i = 0; i < 17; i++)
-      result[i] = Wire.read();
-
-    subsystem_status = (subsystem_status & 0b110) | ((result[0] == 0x01) << 0);
-
-    // Turns 4-Byte data into float
-
-    tmp = ((uint32_t)result[4] << 24) | ((uint32_t)result[3] << 16) | ((uint32_t)result[2] << 8) | ((uint32_t)result[1]);
-    height_pressure = *(float*)&tmp;
-
-    tmp = ((uint32_t)result[8] << 24) | ((uint32_t)result[7] << 16) | ((uint32_t)result[6] << 8) | ((uint32_t)result[5]);
-    height_gnss = *(float*)&tmp;
-
-    tmp = ((uint32_t)result[12] << 24) | ((uint32_t)result[11] << 16) | ((uint32_t)result[10] << 8) | ((uint32_t)result[9]);
-    lat_gnss = *(float*)&tmp;
-
-    tmp = ((uint32_t)result[16] << 24) | ((uint32_t)result[15] << 16) | ((uint32_t)result[14] << 8) | ((uint32_t)result[13]);
-    lon_gnss = *(float*)&tmp;
-  }
-
-  // Sensor circuit board 2: status (1 Byte), acceleration (4 Bytes)
-  if((i2c_connections & 0b010) != 0)
-  {
-    Wire.requestFrom(0x30, 5); 
-    uint8_t result[5];
-    for(int i = 0; i < 5; i++)
-      result[i] = Wire.read();
-
-    subsystem_status = (subsystem_status & 0b101) | ((result[0] == 0x01) << 1);
-
-    // Turns 4-Byte data into float
-
-    tmp = ((uint32_t)result[4] << 24) | ((uint32_t)result[3] << 16) | ((uint32_t)result[2] << 8) | ((uint32_t)result[1]);
-    acceleration = *(float*)&tmp;
-  }
-
-  // Landing systems: status (1 Byte), status events (1 Byte)
-  if((i2c_connections & 0b100) != 0)
-  {
-    Wire.requestFrom(0x40, 2); 
-    uint8_t result[2];
-    for(int i = 0; i < 2; i++)
-      result[i] = Wire.read();
-
-    subsystem_status = (subsystem_status & 0b011) | ((result[0] == 0x01) << 2);
-
-    status_events = result[1];
-  }
-
-  // Battery_voltage (Voltage divider)
-  // battery_voltage = (float)analogRead(d2pin) / 1023 * 3.3 * (18 + 10) / 10; Will not be used anymore due to power unit doing the measurement
-  battery_voltage = 0;
-
-  // Temperature
-  // rc1780hp.read_Temperature(&temperature);
-}
-
-// Encodes current data into 15-Byte packet and transmits it
-void send_packet()
-{
-  uint8_t packet[15] = { 0 };
-  Packet::encode(packet, (float)temperature, subsystem_status, flight_mode, low_power_mode, status_events, acceleration, height_pressure, height_gnss, lat_gnss, lon_gnss, battery_voltage);
-  SerialModule1->write(packet, 15);
+  return 0;
 }
